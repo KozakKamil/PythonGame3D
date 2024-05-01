9bb55ceb334e2aa62079b4dd410c1b43b2e2178e
Kamil Kozak <kozak4990@gmail.com>
2024-05-01 10:00:51 +0200

    adding and removing voxels

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ class PythonGame:
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 self.is_running = False
+            self.player.handle_event(event=event)
 
     def run(self):
         while self.is_running:
